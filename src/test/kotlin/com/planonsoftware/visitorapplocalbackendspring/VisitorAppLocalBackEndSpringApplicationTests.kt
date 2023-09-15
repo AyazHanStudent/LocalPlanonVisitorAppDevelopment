ccd9222bda2eb72e2108b06f8ca10f889d680b0d
AyazHanStudent <93929037+AyazHanStudent@users.noreply.github.com>
2023-09-15 15:15:42 +0200

    Initial commit on main branch

---
+++ src/test/kotlin/com/planonsoftware/visitorapplocalbackendspring/VisitorAppLocalBackEndSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.planonsoftware.visitorapplocalbackendspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VisitorAppLocalBackEndSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
